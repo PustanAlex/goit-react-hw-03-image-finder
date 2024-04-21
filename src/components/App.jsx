2a0b6f65eb58df34e98f69a86ca089d8f25845d4
PustanAlex <alecsandru_bogdy@yahoo.com>
2024-04-21 15:29:51 +0300

    Fetch API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+class App extends Component {
+  componentDidMount() {
+    const API_KEY = '43501408-f2ca706ad0af36d7ba8bbed94'
+    const URL = `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+
+    fetch(URL) 
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error fetching data', error));
+  }
+
+  render() {
+  
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
